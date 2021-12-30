bc7a1d233d7d4cfaf34023bb8055c6769886c557
Braian Vaylet <braianvaylet@gmail.com>
2021-12-30 16:15:46 -0300

    reto4

---
+++ #4/index.js
@@ -0,0 +1,19 @@
+function createXmasTree(height) {
+  const data = []
+
+  for (let index = 1; index <= height; index++) {
+    const row =
+      '_'.repeat(height - index) +
+      '*'.repeat(index * 2 - 1) +
+      '_'.repeat(height - index)
+    data.push(row)
+  }
+
+  const trunk = '_'.repeat(height - 1) + '#' + '_'.repeat(height - 1)
+  data.push(trunk)
+  data.push(trunk)
+
+  return data.join('\n')
+}
+
+console.log(createXmasTree(10))
